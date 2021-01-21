ace248dc5ef2be298760bc0ad7f5349e9f5747ef
Maks-s <karlmaks-s@protonmail.com>
2021-01-21 18:39:22 +0100

    :building_construction: Rename project to 'learnproject'

--- learnproject/urls.py
+++ learnproject/urls.py
@@ -1,4 +1,4 @@
-"""djpytest URL Configuration
+"""learnproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
