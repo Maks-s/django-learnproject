2c88685877ec7a6fa2b7db56ce82e1c3a0e888e9
Maks-s <karlmaks-s@protonmail.com>
2021-01-22 18:40:03 +0100

    :white_check_mark: Add all tests for forums

--- forum/tests.py
+++ forum/tests.py
@@ -3,33 +3,90 @@ from django.test import Client, TestCase
 from django.urls import reverse
 
 from .models import Post
+from .forms import PostForm
 
 class TestForum(TestCase):
-    def setUp(self):
-        self.client = Client()
+    # Global data used across all tests
+    @classmethod
+    def setUpTestData(cls):
+        super(TestForum, cls).setUpTestData()
 
-        self.user = User.objects.create_user('Bob')
-        self.superuser = User.objects.create_superuser('root')
+        cls.user = User.objects.create_user('Bob')
+        cls.superuser = User.objects.create_superuser('root')
 
-        self.post = Post.objects.create(
-            author = self.user,
+        cls.post = Post.objects.create(
+            author = cls.user,
             title = 'test',
             message = 'This is a test message to test things out'
         )
 
-class TestTemplates(TestForum):
-    def http_code_test(self, url, code=200):
+        cls.url_post_list = reverse('forum:post-list')
+        cls.url_post_create = reverse('forum:post-create')
+        cls.url_post_detail = reverse('forum:post-detail', args=(cls.post.id,))
+
+    def assertGetHTTPCode(self, url, code=200):
         return self.assertEqual(self.client.get(url).status_code, code)
 
-    def test_templates(self):
-        url_post_list = reverse('forum:post-list')
-        url_post_create = reverse('forum:post-create')
-        url_post_detail = reverse('forum:post-detail', args=[self.post.id])
+    def assertPostHTTPCode(self, url, data={}, code=200):
+        return self.assertEqual(self.client.post(url, data).status_code, code)
 
-        self.http_code_test(url_post_list, 200)
-        self.http_code_test(url_post_detail, 200)
-        self.http_code_test(url_post_create, 302)
+# Generic tests
+class TestTemplates(TestForum):
+    def test_templates(self):
+        self.assertGetHTTPCode(self.url_post_list, 200)
+        self.assertGetHTTPCode(self.url_post_detail, 200)
+
+# Views tests
+class TestPostCreateView(TestForum):
+    def test_post_authorization(self):
+        self.assertGetHTTPCode(self.url_post_create, 302)
 
         self.client.force_login(self.user)
 
-        self.http_code_test(url_post_create, 200)
+        self.assertGetHTTPCode(self.url_post_create, 200)
+
+    def test_post_creation(self):
+        self.client.force_login(self.user)
+        data = {
+            'title': 'Creation test',
+            'message': 'Message'
+        }
+
+        self.assertPostHTTPCode(self.url_post_create, data, 302)
+
+        Post.objects.get(title=data['title'])
+
+    def test_invalid_post_creation(self):
+        self.client.force_login(self.user)
+        data = {
+            'title': '',
+            'message': ''
+        }
+
+        self.assertPostHTTPCode(self.url_post_create, data, 200)
+
+        with self.assertRaises(Post.DoesNotExist):
+            Post.objects.get(title=data['title'])
+
+# Forms tests
+class TestPostForm(TestForum):
+    def test_valid_create_form(self):
+        form = PostForm(
+            data = {
+                'title': 'Nice',
+                'message': 'Nice',
+                'author': self.user
+            }
+        )
+
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_create_form(self):
+        form = PostForm(
+            data = {
+                'title': '',
+                'message': ''
+            }
+        )
+
+        self.assertFalse(form.is_valid())
